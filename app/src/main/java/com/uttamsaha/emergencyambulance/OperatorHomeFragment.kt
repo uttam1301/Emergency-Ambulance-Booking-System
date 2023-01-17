5b62f91cc5af77f373be8854dbc7ef99ccbd8682
Uttam Kumar Saha <uttam.saha2019@gift.edu.n>
2023-01-17 22:13:12 +0530

    Operator Location Done

--- app/src/main/java/com/uttamsaha/emergencyambulance/OperatorHomeFragment.kt
+++ app/src/main/java/com/uttamsaha/emergencyambulance/OperatorHomeFragment.kt
@@ -1,32 +1,46 @@
 package com.uttamsaha.emergencyambulance
 
+import android.content.res.Resources
+import android.location.LocationRequest
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import java.util.jar.Manifest
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class OperatorHomeFragment : Fragment(), OnMapReadyCallback {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OperatorHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class OperatorHomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment : SupportMapFragment
+    private lateinit var locationRequest : com.google.android.gms.location.LocationRequest
+    private lateinit var locationCallback : LocationCallback
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onDestroy() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        super.onDestroy()
     }
 
     override fun onCreateView(
@@ -34,26 +48,85 @@ class OperatorHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operator_home, container, false)
+
+        val root = inflater.inflate(R.layout.fragment_operator_home, container, false)
+
+        init()
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OperatorHomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OperatorHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun init() {
+        locationRequest = com.google.android.gms.location.LocationRequest()
+        locationRequest.priority
+        locationRequest.setFastestInterval(3000)
+        locationRequest.interval = 5000
+        locationRequest.setSmallestDisplacement(10f)
+
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                val newPos = LatLng(locationResult.lastLocation!!.latitude,locationResult.lastLocation!!.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos,18f))
             }
+        }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,Looper.myLooper())
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        Dexter.withContext(requireContext()).withPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(object : PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+
+                    mMap.isMyLocationEnabled = true
+                    mMap.uiSettings.isMyLocationButtonEnabled = true
+                    mMap.setOnMyLocationClickListener {
+                        fusedLocationProviderClient.lastLocation
+                            .addOnFailureListener { e->
+                                Toast.makeText(context!!, e.message,Toast.LENGTH_SHORT).show()
+                            }.addOnSuccessListener { location->
+                                val userLatLng = LatLng(location.latitude,location.longitude)
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng,18f))
+                            }
+                        true
+                    }
+
+                    mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                    val view = mapFragment!!.requireView()
+                        .findViewById<View>("1".toInt())!!
+                        .parent!! as View
+                    val locationButton = view.findViewById<View>("2".toInt());
+                    val params = locationButton.layoutParams as RelativeLayout.LayoutParams
+                    params.addRule(RelativeLayout.ALIGN_TOP,0)
+                    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE)
+                    params.bottomMargin = 50
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(context!!, "Permission"+p0!!.permissionName+"was denied",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+
+                }
+
+            }).check()
+        try {
+            val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(),R.raw.ambulance_maps_style))
+            if(!success)
+                Log.e("EDMT_ERROR","Style parsing error")
+        }catch (e: Resources.NotFoundException){
+            e.message?.let { Log.e("EDMT_ERROR", it) }
+        }
     }
 }
